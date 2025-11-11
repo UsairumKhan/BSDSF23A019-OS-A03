8cdbbba74d061c558232f87173f3d610a55f3b54
UsairumKhan <usairumkhan999@gmail.com>
2025-11-11 19:03:04 +0500

    version final imlplemented environmental variables.

--- include/shell.h
+++ include/shell.h
@@ -15,24 +15,30 @@
 
 #define MAX_LEN 1024
 #define MAXARGS 100
-#define ARGLEN 100
+#define ARGLEN 256
 #define PROMPT "myshell> "
-#define HISTORY_SIZE 50
-
-/* Function declarations */
+#define HISTORY_SIZE 200
+#define MAX_VARS 200
 
 /* Input */
 char* read_cmd(char* prompt);
 
 /* Parsing & execution */
 char** tokenize(char* cmdline);
-int execute(char* arglist[], char* fullcmd);
+int execute(char* arglist[]);
 int handle_builtin(char** arglist);
+
+/* If-else handling */
 int handle_if_else(char* cmdline);
 
 /* Internal history */
 void internal_history_add(const char* cmdline);
 void internal_history_show();
 char* internal_history_get(int index);
+
+/* Variables (session) */
+void set_var(const char* name, const char* value);
+const char* get_var(const char* name);
+void expand_token_inplace(char* dest, const char* src);
 
 #endif
